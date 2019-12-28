ba512b56a9447e3f879ee1fbb85df2b3fb812197
Wilson <Wilopez95@gmail.com>
2019-12-28 11:15:52 -0600

    Nueva activity de registro a aactividad

---
+++ app/src/main/java/com/example/teconecta/Actividad_Register.java
@@ -0,0 +1,14 @@
+package com.example.teconecta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Actividad_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad__register);
+    }
+}

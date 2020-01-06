efbc8acf8842f413d81d67d645f044c71e7c1083
Wilson <Wilopez95@gmail.com>
2020-01-06 15:18:34 -0600

    Se agrego la ventana para el registro a las actividades

--- app/src/main/java/com/example/teconecta/Actividad_Register.java
+++ app/src/main/java/com/example/teconecta/Actividad_Register.java
@@ -3,12 +3,53 @@ package com.example.teconecta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Actividad_Register extends AppCompatActivity {
+    private EditText name;
+    private EditText passport;
+    private EditText email;
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad__register);
+
+        name = findViewById(R.id.name);
+        passport = findViewById(R.id.passport);
+        email = findViewById(R.id.email);
+
+        button= findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkfields()){
+                    //Registra
+                    Toast.makeText(getApplicationContext(), "¡Su registro a la actividad se realizao con exito!", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(), "¡Datos incorrectos!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+
+
+    private boolean checkfields(){
+        if(name.getText().toString().isEmpty()|| passport.getText().toString().isEmpty() || email.getText().toString().isEmpty()){
+            return false;
+        }else {
+            return true;
+        }
     }
 }
